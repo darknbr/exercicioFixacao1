9d56adb0133b706bbd6118af209db179a77cb848
Danillo Dias <darknbr@gmail.com>
2023-08-08 14:09:33 -0300

    V0.0

---
+++ src/main/java/br/com/mentorama/exercicioFixacao1/ExercicioFixacao1Application.java
@@ -0,0 +1,13 @@
+package br.com.mentorama.exercicioFixacao1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioFixacao1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioFixacao1Application.class, args);
+	}
+
+}
